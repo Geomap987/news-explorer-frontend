4e52a6e7b6460189adb3cbd03c682f67cd47f461
Geomap987 <buziuka@yandex.ru>
2021-01-14 01:16:23 +0300

    JSX, CSS, utils

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,6 +1,4 @@
-  
-import React, { useState, useCallback, useEffect } from 'react';
-
+import { useState, useEffect } from 'react';
 
 const useValidation = (value, validations) => {
 
@@ -8,8 +6,6 @@ const useValidation = (value, validations) => {
     const [minLengthError, setMinLengthError] = useState(false)
     const [maxLengthError, setMaxLengthError] = useState(false)
     const [isEmailError, setEmailError] = useState(false)
-    const [isContainNumbersError, setNumberError] = useState(false)
-    const [isFullNameError, setFullNameError] = useState(false)
     const [inputValid, setInputValid] = useState(false)
 
     useEffect(() => {
@@ -76,32 +72,4 @@ const useInput = (initialValue, validations) => {
     }
 }
 
-
-function ContactForm(inputValid) {
-
-    const fullName = useInput('', { isEmpty: true, minLength: 5, isFullName: true })
-    const email = useInput('', { isEmpty: true, isEmail: true, maxLength: 40 })
-    const phoneNumber = useInput('', { isEmpty: true, isContainNumbers: true, maxLength: 12 })
-    const poems = useInput('', { isEmpty: true, minLength: 2 })
-
-
-    return (
-        <article className="form">
-            <h2 className="form__title">ФОРМА.</h2>
-            <h3 className="form__subtitle">Заполняя эту форму, вы становитесь частью проекта.</h3>
-            <form name="form-turbina" className="form__container" action="#">
-                <input className="form__input" onChange={e => fullName.onChange(e)} onBlur={e => fullName.onBlur(e)} value={fullName.value} name="fullName" type="text" placeholder="Имя и фамилия автора"
-                />
-                {(fullName.isDirty && fullName.isFullNameError && fullName.isEmpty) && <div style={{ color: 'red' }}>Поле обязательно для заполнения (В формате: А-я)</div>}
-                {(fullName.isDirty && fullName.minLengthError) && <div style={{ color: 'red' }}>Минимальная длина: 5 символов</div>}
-                <input className="form__input" onChange={email.onChange} onBlur={email.onBlur} value={email.value} name="email" type="email" placeholder="Почта" />
-                {(email.isDirty && email.isEmpty) && <div style={{ color: 'red' }}>Поле обязательно для заполнения</div>}
-                {(email.isDirty && email.isEmailError) && <div style={{ color: 'red' }}>Некорректный e-mail</div>}
-                {(email.isDirty && email.maxLengthError) && <div style={{ color: 'red' }}>Максимальная длина: 40 символов</div>}
-        
-                <button disabled={!fullName.inputValid || !phoneNumber.inputValid || !email.inputValid || !poems.inputValid} className="form__submit-button">Отправить форму</button>
-            </form>
-        </article>
-    );
-}
 export {useValidation, useInput} 
